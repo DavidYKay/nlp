f1a6fb6ad12bc289a9ac739f664b3751d8a86740
David Young-chan Kay <davidykay@gmail.com>
2016-02-23 10:11:02 -0800

    implemented word frequency

--- src/nlp/core.clj
+++ src/nlp/core.clj
@@ -4,16 +4,20 @@
 (defn read-file [f]
   (slurp f))
 
-(defn word-count [s]
+(defn word-count-raw [s]
   (->> (remove blank? (split s #"\s"))
        (group-by identity )
        (map (fn [[k v]] [k (count v)]))
        (sort-by last)
        (reverse)))
+(def word-count (memoize word-count-raw))
 
 (defn word-frequency [s]
-
-  )
+  (->> (word-count s)
+      (group-by last)
+      (map (fn [[occurrences words]]
+             {:occurrences occurrences
+              :num-words (count words)}))))
 
 (defn zipfs-law [f]
 
